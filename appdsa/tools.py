61c6a256471b48e9715c37d40a10cad6f5a00be0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-10 14:41:13 +0100

    Implement num_floor()

--- appdsa/tools.py
+++ appdsa/tools.py
@@ -15,3 +15,21 @@ def num_truncate(value: float) -> int:
     -9
     """
     return int(value)
+
+
+def num_floor(value: float) -> int:
+    """Return the floor approximation of value
+    
+    Paramters
+    ---------
+    value: float
+    
+    Examples
+    --------
+    >>> num_floor(3.1)
+    3
+    >>> num_floor(-3.1)
+    -4
+    """
+    value = (value - 1) if value < 0 else value
+    return int(value)
