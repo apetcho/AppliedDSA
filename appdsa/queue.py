6d9daecb48c407b8fd3b493badc23e5428394575
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-10 20:28:51 +0100

    Add __str__ method for Queue class

--- appdsa/queue.py
+++ appdsa/queue.py
@@ -1,15 +1,21 @@
 """Implementation of stack data structure using a singly linked list."""
-
+from .snode import Node
 
 class Queue:
     """Queue Data Structure."""
 
     def __init__(self):
-        self._front = None
-        self._back = None
+        self._first : Node = None
+        self._last : Node = None
 
     def __str__(self) -> str:
-        pass
+        items = []
+        node = self._first
+        while not (node is None):
+            items.append(node.data)
+            node = node.next
+        return str(items)
+
 
     def is_empty(self):
         pass
