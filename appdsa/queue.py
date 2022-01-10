210cb3e45c9730116a1ae1b70d63779b0ff50c87
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-10 20:25:02 +0100

    Define a queue data structure interface using a singly linked

---
+++ appdsa/queue.py
@@ -0,0 +1,21 @@
+"""Implementation of stack data structure using a singly linked list."""
+
+
+class Queue:
+    """Queue Data Structure."""
+
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    def __str__(self) -> str:
+        pass
+
+    def is_empty(self):
+        pass
+
+    def enqueue(self, data):
+        pass
+
+    def dequeue(self):
+        pass
