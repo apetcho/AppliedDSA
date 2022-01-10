9bf8d86659cb881370ae8c9016f2dcacb578f474
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-10 20:07:31 +0100

    Define a simple singly linked list node interface

---
+++ appdsa/snode.py
@@ -0,0 +1,30 @@
+"""Implementation of a singly linked list node."""
+
+class Node:
+    """Singly linked list node."""
+
+    def __init__(self, data=None, next=None):
+        self._data = data
+        self._next = next
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    @property
+    def data(self):
+        pass
+
+    @data.setter
+    def data(self, value):
+        pass
+
+    @property
+    def next(self):
+        pass
+
+    @next.setter
+    def next(self, link):
+        pass
