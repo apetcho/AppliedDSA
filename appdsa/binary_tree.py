a66912622824373cb6aa14bceaf83bca740605c9
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-10 21:39:41 +0100

    Define a BST and its node interface

---
+++ appdsa/binary_tree.py
@@ -0,0 +1,106 @@
+"""An Implementation of Binary Tree."""
+from typing import Any
+
+class Node:
+    """Binary Tree node."""
+
+    def __init__(self, data: Any, left: "Node"=None, right: "Node"=None):
+        pass
+
+    def display(self, indent=0):
+        pass
+
+    def search(self, data: Any):
+        pass
+
+    def insert(self, data: Any):
+        pass
+
+    def largest(self):
+        pass
+
+    def smallest(self):
+        pass
+
+    @property
+    def data(self):
+        pass
+
+    @data.setter
+    def data(self, value):
+        pass
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def num_nodes(self):
+        pass
+
+    def search_parent(self, data):
+        pass
+
+    def delete_node(self, data, parent):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def display_preorder(self):
+        pass
+
+    def _preorder(self, container):
+        pass
+
+    def preorder(self):
+        pass
+
+    def _postorder(self, container):
+        pass
+
+    def postorder(self):
+        pass
+
+
+class BinarySearchTree:
+    """Binary Search Tree class."""
+
+    def __init__(self):
+        pass
+
+    def display(self):
+        pass
+
+    def search(self, data):
+        pass
+
+    def insert(self, data):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def preorder(self):
+        pass
+
+    def display_preorder(self):
+        pass
+
+    def postorder(self):
+        pass
+
+    def display_postorder(self):
+        pass
+
+    def num_nodes(self):
+        pass
+
+
+def _test():
+    pass
+
+
+if __name__ == "__main__":
+    _test()
