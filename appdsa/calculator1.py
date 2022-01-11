1476478ae47523b32ac123cc70ff3a9e5af1f3cc
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-11 01:06:18 +0100

    Define skeleton for arithmetic calculator app

---
+++ appdsa/calculator1.py
@@ -0,0 +1,83 @@
+"""Simple arithmetic calculator using rational.py module and a binary tree."""
+
+class Lexer:
+
+    def __init__(self):
+        pass
+
+    def __call__(self, textline: str) -> list:
+        pass
+
+
+class ExprNode:
+    """Expression node."""
+
+    def __init__(self):
+        pass
+
+    def make_prefix_expr(self):
+        pass
+
+    def make_infix_expr(self):
+        pass
+
+    def make_postfix_expr(self):
+        pass
+
+    @property
+    def term(self):
+        pass
+
+
+def simplify_parenthesis(textline: str) -> str:
+    pass
+
+
+class ExpressionTree:
+    """Binary tree representing an expression."""
+
+    def __init__(self, infix: str):
+        pass
+
+    def new_operation(self):
+        pass
+
+    def priority(self, op: str):
+        pass
+
+    def prefix(self):
+        pass
+
+    def infix(self):
+        pass
+
+    def postfix(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+
+class Application:
+
+    def __init__(self, expression: str):
+        pass
+
+    def _calculate(self):
+        pass
+
+    def demos(self):
+        pass
+
+    def _read_expr(self):
+        pass
+
+    def _mainloop(self):
+        pass
+
+    def __call__(self):
+        pass
+
+
+def main():
+    pass
