23b628e38a8bc6bcba608091037712347d9b0e0a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-20 19:40:28 +0100

    Implement htbale_create().

--- cdsa/src/htable.c
+++ cdsa/src/htable.c
@@ -1,3 +1,6 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
 #include "htable.h"
 
 //
@@ -7,4 +10,29 @@ static unsigned int hash(const char *x){
         h = h * 65599 + (unsigned char)x[i];
     }
     return h & 1023;
+}
+
+static int check_htable(HTable *table){
+    if(table == NULL){
+        return -1;
+    }
+    return 0;
+}
+
+static int check_hnode(HNode *node){
+    if(node == NULL){ return -1; }
+    return 0;
+}
+
+//
+HTable* htable_create(void){
+    HTable *table;
+
+    table = (HTable*)calloc(1, sizeof(*table));
+    if(check_htable(table) == -1){
+        perror("htable_create(): allocation failed.");
+        return  NULL;
+    }
+
+    return table;
 }
